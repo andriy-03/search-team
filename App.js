81effcbf8976e6b9780a449911467ee97ddc81ce
andriy-03 <126471164+andriy-03@users.noreply.github.com>
2023-03-14 19:31:03 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar"
+import About from "./components/About"
+import CardTeam from "./components/CardTeam";
+import VideoPlayer from "./components/VideoPlayer";
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={ <Navbar /> }></Route>
+        <Route path="/about" element={ <About /> }></Route>
+        <Route path="/teams/:teamId" element={ <CardTeam />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App
